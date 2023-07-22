be35bfffea4c65ac2dc6f5380985e2e613c5d415
guarando <pawelpurzyk@gmail.com>
2023-07-22 12:47:22 +0200

    Remove templating script

--- templating.py
+++
@@ -1,9 +0,0 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-env = Environment(
-    loader=PackageLoader("main"),
-    autoescape=select_autoescape()
-)
-
-template = env.get_template("toc.ncx")
-
-print(template.render(title="SAMPLE_TITLE", articles=["a1", "ART2"]))
